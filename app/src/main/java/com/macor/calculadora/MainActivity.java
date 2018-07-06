20ae3906c9d8e32610c91fd058f4c06c32edffd6
Mario Ortega <mcor63@gmail.com>
2018-07-05 22:39:50 -0400

    Se agrega metodo para ingresar numeros en pantalla, tambien se agrega efecto ripple para botones.

--- app/src/main/java/com/macor/calculadora/MainActivity.java
+++ app/src/main/java/com/macor/calculadora/MainActivity.java
@@ -14,14 +14,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void asignarValor(View v) {
+        Button bvalor = (Button) v;
+        String valor = bvalor.getText().toString();
+
+        //if (digit1 > 0) {
+        //digit2 = Integer.parseInt(valor);
+        //} else {
+        //  digit1 = Integer.parseInt(valor);
+        //}
+        //Toast.makeText(this, "Digit1: " + digit1 + " Digit2: " + digit2, Toast.LENGTH_SHORT).show();
+        //TextView display = (TextView) findViewById(R.id.display);
+        //display.append(valor);
+    }
+
     public void addNumber(View view) {
-        final Button btnNumber = findViewById(R.id.number7);
+        final Button btnNumber = (Button) view;
+        final String valor = btnNumber.getText().toString();
         final TextView txtOperation = findViewById(R.id.txtOperation);
 
         btnNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtOperation.append("7");
+                txtOperation.append(valor);
             }
         });
     }
@@ -30,12 +45,12 @@ public class MainActivity extends AppCompatActivity {
         final Button btnClear = findViewById(R.id.clear);
         final TextView txtOperation = findViewById(R.id.txtOperation);
 
-        btnClear.setOnClickListener(new View.OnClickListener(){
+        btnClear.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                String text = txtOperation.toString();
-                text = text.substring(0,text.length()-1);
+                String text = txtOperation.getText().toString();
+                text = text.substring(0, text.length() - 1);
                 txtOperation.setText(text);
             }
         });
