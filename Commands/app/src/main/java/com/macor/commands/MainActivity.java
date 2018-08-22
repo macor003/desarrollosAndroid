2fa16d7ab3ef300f6531d207172e0d7153db734b
Mario Ortega <mcor63@gmail.com>
2018-08-22 16:30:28 -0400

    Se agrega petición a api rest

--- Commands/app/src/main/java/com/macor/commands/MainActivity.java
+++ Commands/app/src/main/java/com/macor/commands/MainActivity.java
@@ -1,7 +1,13 @@
 package com.macor.commands;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void login (View v){
+
+        EditText txtUser = findViewById(R.id.txtUser);
+        EditText txtPass = findViewById(R.id.txtPass);
+
+        String user = txtUser.getText().toString();
+        String pass = txtPass.getText().toString();
+
+        Intent intent = new Intent(this, HomeActivity.class);
+
+        startActivity(intent);
+    }
+
 }
