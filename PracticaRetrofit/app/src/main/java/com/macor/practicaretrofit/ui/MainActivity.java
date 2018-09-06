58c36a9e937626a75bc03013b9f35df23449fe04
Mario Ortega <mcor63@gmail.com>
2018-09-06 12:14:20 -0400

    Se agregan imagen, logo y modelo de comandas

--- PracticaRetrofit/app/src/main/java/com/macor/practicaretrofit/ui/MainActivity.java
+++ PracticaRetrofit/app/src/main/java/com/macor/practicaretrofit/ui/MainActivity.java
@@ -1,21 +1,27 @@
 package com.macor.practicaretrofit.ui;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import io.futurestud.retrofit1.R;
-
+import com.becoblohm.cr.models.Order;
 import com.becoblohm.cr.net.response.RMIServerResponse;
-
+import com.macor.practicaretrofit.api.model.Commands;
 import com.macor.practicaretrofit.api.service.ServiceClient;
 import com.macor.practicaretrofit.ui.adapter.CommandsAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.futurestud.retrofit1.R;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,13 +30,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
 
+    private ListView listView;
     private RecyclerView rView;
     private RecyclerView.Adapter adapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -40,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         rView.setHasFixedSize(true);
         rView.setLayoutManager(new LinearLayoutManager(this));
 
-        Retrofit.Builder builder = new Retrofit.Builder()
+        final Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("http://10.1.10.45:1092")
                 .addConverterFactory(GsonConverterFactory.create());
 
@@ -54,20 +62,60 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call<RMIServerResponse> call, Response<RMIServerResponse> response) {
-                RMIServerResponse responseAnulDev = response.body();
+                List<Order> responseList = (List<Order>) response.body().getData();
                 Log.i("Información", "Llego la petición *******************");
 
+                List<Commands> commandsList = new ArrayList<>();
+
+                commandsList.add(new Commands("CAROLINA SOLARES", 326377));
+                commandsList.add(new Commands("FERNANDO ORELLANA", 326379));
+                commandsList.add(new Commands("EDGAR MORA", 326384));
+                commandsList.add(new Commands("ANTONIO PEREZ", 326385));
+                commandsList.add(new Commands("WILLIAM PAIZ", 326390));
+
                 progress.dismiss();
-                rView.setAdapter(new CommandsAdapter(MainActivity.this, responseAnulDev));
+                rView.setAdapter(new CommandsAdapter(MainActivity.this, commandsList));
             }
 
             @Override
             public void onFailure(Call<RMIServerResponse> call, Throwable t) {
-
                 Log.i(TAG, t.getMessage());
                 progress.dismiss();
-                Toast.makeText(MainActivity.this, "error :(", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "error :(", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builderAlert = new AlertDialog.Builder(MainActivity.this);
+                builderAlert.setTitle("Error").setMessage("Error en la conexión con el servidor");
+                builderAlert.setIcon(R.mipmap.ic_error);
+                builderAlert.setPositiveButton("Reintentar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        restartActivity(MainActivity.this);
+                    }
+                });
+                builderAlert.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+                AlertDialog dialog = builderAlert.create();
+                dialog.show();
+
+
             }
         });
     }
+
+    public static void restartActivity(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            activity.recreate();
+        } else {
+            activity.finish();
+            activity.startActivity(activity.getIntent());
+        }
+    }
+
 }
+
+
--- PracticaRetrofit/app/src/main/java/com/macor/practicaretrofit/ui/adapter/CommandsAdapter.java
+++ PracticaRetrofit/app/src/main/java/com/macor/practicaretrofit/ui/adapter/CommandsAdapter.java
@@ -3,18 +3,13 @@ package com.macor.practicaretrofit.ui.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import com.becoblohm.cr.models.Order;
-import com.becoblohm.cr.net.response.RMIServerResponse;
-import com.becoblohm.cr.net.response.ServicesResponse;
+import com.macor.practicaretrofit.api.model.Commands;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import io.futurestud.retrofit1.R;
@@ -23,12 +18,12 @@ public class CommandsAdapter extends RecyclerView.Adapter<CommandsAdapter.ViewHo
 
 
     private Context context;
-    private RMIServerResponse values;
+    private List<Commands> values;
 
-    public CommandsAdapter(Context context, RMIServerResponse responseAnulDev) {
+    public CommandsAdapter(Context context, List<Commands> response) {
 
         this.context = context;
-        this.values = responseAnulDev;
+        this.values = response;
     }
 
     @NonNull
@@ -42,21 +37,13 @@ public class CommandsAdapter extends RecyclerView.Adapter<CommandsAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-        Log.i("onBindViewHolder", "Entro al metodo onBindViewHolder");
-
-        ServicesResponse response = new ServicesResponse();
-        response.setData(values.getData());
-        List<Order> orders;
-        orders = (List<Order>) response.getData();
-        Order order = orders.get(position);
-
-        holder.showDetails(order);
+        Commands call = values.get(position);
+        holder.showDetails(call);
     }
 
     @Override
     public int getItemCount() {
-        return 1;
+        return values.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -72,9 +59,9 @@ public class CommandsAdapter extends RecyclerView.Adapter<CommandsAdapter.ViewHo
 
         }
 
-        public void showDetails(Order order) {
-            String name = order.getClient().getName();
-            String number = order.getClient().getIdNumber();
+        public void showDetails(Commands order) {
+            String name = order.getName();
+            String number = String.valueOf(order.getNumber());
             nameTxtView.setText(name);
             numberTxtView.setText(number);
 
